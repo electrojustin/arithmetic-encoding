4014a1d8ffea4e98a98297d6095d90f3ca7665d2
Justin Green <jeg112@case.edu>
2023-03-26 11:02:56 -0400

    Added some more comments

--- markov_decoder.py
+++ markov_decoder.py
@@ -2,6 +2,10 @@ from arithmetic_decoder import ArithmeticDecoder
 from bit_utils import bits_to_bytes, bytes_to_bits
 import sys
 
+# Implement an arithmetic decoder that assumes that the probability of the next
+# byte is influenced by the previous byte, like a Markov chain. This has good
+# performance for some data, like English text files.
+
 input_bytes = []
 with open(sys.argv[1], 'rb') as input_file:
   input_bytes = input_file.read()
@@ -10,15 +14,16 @@ probs = []
 for i in range(0, 256):
   probs.append([])
   for j in range(0, 256):
-    probs[-1].append(1)
+    probs[-1].append(0)
 
+# Probability normalization logic that makes sure every outcome has a
+# 1/65536 chance of happening. I was worried that numbers which are "too small"
+# will result in floating point issues.
 def normalize_probs(probs):
   ret = probs.copy()
-
   total = sum(ret)
   if total >= 2**16:
     ret = list(map(lambda x: x*(2**16)/total, ret))
-
   ret = list(map(lambda x: x if x else 1, ret))
   total = sum(ret)
   ret = list(map(lambda x: x / total, ret))
--- simple_encoder.py
+++ simple_encoder.py
@@ -1,21 +1,25 @@
 from arithmetic_encoder import ArithmeticEncoder
 import sys
 
+# Implement an arithmetic encoder that assumes that bytes are distributed
+# independently and randomly.
+
 input_bytes = []
 with open(sys.argv[1], 'rb') as input_file:
   input_bytes = input_file.read()
 
 probs = []
 for i in range(0, 256):
-  probs.append(1)
+  probs.append(0)
 
+# Probability normalization logic that makes sure every outcome has a
+# 1/65536 chance of happening. I was worried that numbers which are "too small"
+# will result in floating point issues.
 def normalize_probs(probs):
   ret = probs.copy()
-
   total = sum(ret)
   if total >= 2**16:
     ret = list(map(lambda x: x*(2**16)/total, ret))
-
   ret = list(map(lambda x: x if x else 1, ret))
   total = sum(ret)
   ret = list(map(lambda x: x / total, ret))
